49ea8136f9ebafba09e3c1cd9d46ba545fa693ad
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-04-28 20:07:49 +0200

    add date

--- src/components/SummaryCard.js
+++ src/components/SummaryCard.js
@@ -1,25 +1,23 @@
-import moment from "moment";
-
-const SummaryCard = ({ day }) => {
-  let dayIcon = `${
-    process.env.REACT_APP_WEATHER_ICON + day.weather[0]["icon"]
+const SummaryCard = ({ today }) => {
+  const dayIcon = `${
+    process.env.REACT_APP_WEATHER_ICON_URL + today.weather[0]["icon"]
   }@2x.png`;
 
   return (
     <li className="container p-4 flex items-center justify-center bg-gray-200 rounded-lg my-auto mr-1">
       <div className="my-auto">
         <p className="font-bold text-3x1 text-pink-600 mb-2">
-          {Math.round(day.main.temp)}&deg;C
+          {Math.round(today.main.temp)}&deg;C
         </p>
         <p className="text-2xl text-gray-800 tracking-widest">
-          {day.weather[0].main}
+          {today.weather[0].main}
           <img src={dayIcon} className="w-1/4 inline" />
         </p>
         <p className="text-gray-400 text-xs uppercase tracking-widest">
-          {day.weather[0].description}
+          {today.weather[0].description}
         </p>
         <p className="tracking-wider">
-          {moment(day.dt_txt).format("dddd hh:mm")}am
+          {new Date(today.dt * 1000).toLocaleTimeString("en-IN")}
         </p>
       </div>
     </li>
