83246a1d0c2096af16c080bbf208d79628a0e601
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-04-29 19:57:16 +0200

    add FiveDaysCard component

---
+++ src/components/FiveDaysCard.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const FiveDaysCard = ({ forecast }) => {
+  const formattedList = forecast.list.reduce((acc, curr) => {
+    const dateTime = new Date(curr.dt * 1000);
+    const day = dateTime.getDate();
+    const time = dateTime.getHours();
+    const today = new Date();
+
+    if (
+      /* !acc.find((d) => d?.day === day && d?.time) && */
+      day !== today.getDate() &&
+      curr.dt_txt.includes("12:00:00")
+    ) {
+      const newDay = {
+        ...curr,
+        day,
+        time,
+      };
+      acc.push(newDay);
+    }
+    return acc;
+  }, []);
+
+  /* console.log(formattedList);
+  const today = new Date();
+  console.log(today.getHours()); */
+
+  return (
+    <div class="overflow-x-auto">
+      <ul className="flex flex-row w-max mb-2">
+        {formattedList.map((days) => (
+          <li className="p-4 flex items-center justify-center bg-gray-200 rounded-lg my-auto mr-3">
+            <div className="my-auto">
+              <p className="font-bold text-3x1 text-pink-600 mb-2">
+                {Math.round(days.main.temp)}&deg;C
+              </p>
+              <p className="text-2xl text-gray-800 tracking-widest">
+                {days.weather[0].main}
+                <img
+                  src={`${
+                    process.env.REACT_APP_WEATHER_ICON_URL +
+                    days.weather[0]["icon"]
+                  }@2x.png`}
+                  className="w-1/4 inline"
+                />
+              </p>
+              <p className="text-gray-400 text-xs uppercase tracking-widest">
+                {days.weather[0].description}
+              </p>
+              <p className="tracking-wider">{days.dt_txt}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FiveDaysCard;
